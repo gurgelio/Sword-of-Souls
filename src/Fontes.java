05cd731233d255439f75c03ad759f901eb1a6fec
Markgabe <mgabriel@id.uff.br>
2018-05-15 14:56:23 -0300

    Menu 1.1.2

--- src/Fontes.java
+++ src/Fontes.java
@@ -4,8 +4,8 @@ import java.awt.Font;
 class Fontes {
 
     static TrueTypeFont Fonte1 = new TrueTypeFont(new Font("Tahoma",Font.PLAIN,50), true);
-    static TrueTypeFont Fonte2 = new TrueTypeFont(new Font("Arial",Font.PLAIN,35), true);
-    static org.newdawn.slick.Color Vermelho = new org.newdawn.slick.Color(204,0,0,255);
+    static TrueTypeFont Fonte2 = new TrueTypeFont(new Font("Verdana",Font.PLAIN,35), true);
+    static org.newdawn.slick.Color Red = new org.newdawn.slick.Color(204,0,0,255);
     static org.newdawn.slick.Color Gold = new org.newdawn.slick.Color(204,153,0,100);
 
     static void ShadowFont(String st, int x, int y, TrueTypeFont f, org.newdawn.slick.Color cor){
@@ -18,6 +18,6 @@ class Fontes {
         f.drawString(x, y + 1, st, cor.darker(0.18f));
         f.drawString(x + 1, y , st, cor.darker(0.18f));
         f.drawString(x, y , st, cor);
-
     }
+
 }
