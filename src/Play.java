7db4e93beeec3e709030666155bebda5e7c3c9cc
Markgabe <mgabriel@id.uff.br>
2018-05-15 21:53:55 -0300

    Larry Added

--- src/Play.java
+++ src/Play.java
@@ -1,33 +1,90 @@
-import org.newdawn.slick.*;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.state.*;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
 
+public class Play extends BasicGameState {
 
-class Play extends BasicGameState{
+    private boolean[][] blocked;
     TiledMap map;
-    Play(int state){
+    Larry player;
+    Camera camera;
+    int mapHeight, mapWidth;
+    int tileHeight, tileWidth;
+    private int stateid;
+
+    public Play(int id) {
+        stateid = id;
     }
 
-    @Override
-    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-        map = new TiledMap("map/mapa.tmx");
-    }
-
-    @Override
-    public void render(GameContainer gc, StateBasedGame sbg, Graphics g){
-        map.render(0,0);
-
-    }
-
-    @Override
-    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
-
-    }
-
-    //retorna o ID do estado do jogo
     @Override
     public int getID() {
-        return 1;
+        return stateid;
     }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        map = new TiledMap("map/mapa.tmx");
+        mapWidth = map.getWidth() * map.getTileWidth();
+        mapHeight = map.getHeight() * map.getTileHeight();
+        tileHeight = map.getTileHeight();
+        tileWidth = map.getTileWidth();
+        player = new Larry(tileWidth, tileHeight);
+        camera = new Camera(mapWidth, mapHeight);
+        blocked = new boolean[map.getWidth()][map.getHeight()];
+        initializeBlocked();
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+        player.update(gc, sbg, delta, this);
+
+        if (gc.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
+            sbg.enterState(0);
+        }
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        camera.translate(g, player);
+        map.render(0, 0);
+        player.render();
+    }
+
+    public boolean isBlocked(float x, float y) {
+        int xBlock = (int) x / map.getTileWidth();
+        int yBlock = (int) y / map.getTileHeight();
+        return blocked[xBlock][yBlock];
+    }
+
+    public Vector2f getHeroPosition() {
+        return player.getpos();
+    }
+
+    public void setHeroPosition(Vector2f pos) {
+        player.setpos(pos);
+    }
+
+    private void initializeBlocked() {
+        for (int l = 0; l < map.getLayerCount(); l++) {
+            String layerValue = map.getLayerProperty(l, "blocked", "false");
+
+            if (layerValue.equals("true")) {
+
+                for (int c = 0; c < map.getWidth(); c++) {
+                    for (int r = 0; r < map.getHeight(); r++) {
+
+                        if (map.getTileId(c, r, l) != 0) {
+                            blocked[c][r] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
