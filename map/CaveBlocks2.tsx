66b7c7b02baba3d51985762c90c94889d9d6002f
Markgabe <mgabriel@id.uff.br>
2018-06-14 13:13:11 -0300

    Cave 1.1

---
+++ map/CaveBlocks2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CaveBlocks2" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="cavetiles.png" trans="000000" width="256" height="336"/>
+</tileset>
---
+++ map/CaveTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CaveTiles" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
+ <image source="ProjectUtumno_full.png" trans="000000" width="2048" height="3040"/>
+</tileset>
